08f4c9dba2849e743465b6793b00ec7a432d0d44
fzhang00 <fzhang00@gmail.com>
2015-09-21 11:15:29 -0400

    added two line for splitting pop-up window horizontally

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -96,4 +96,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; setup pop-up window to split horizontally if window width is more
+;; than 100
+(setq split-height-threshold nil)
+(setq split-width-threshold 100)
+
 ;;; custom.el ends here
